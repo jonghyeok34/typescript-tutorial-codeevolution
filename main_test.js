fd9f6a0e4f184ceadf482e36ffcb328067093af1
jonghyeok <jonghyeok34@finotek.co.kr>
2019-12-13 18:15:27 +0900

    remove test files

--- main_test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var message = "Welcome back";
-console.log(message);
-var x = 10;
-var y = 20;
--- main_test.ts
+++
@@ -1,6 +0,0 @@
-export {};
-let message = "Welcome back";
-console.log(message);
-
-let x = 10;
-const y = 20;
